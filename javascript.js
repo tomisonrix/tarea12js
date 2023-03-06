8b0bacf75761295172185f0f4d6926ce8f4a2ddc
tomisonrix <124640136+tomisonrix@users.noreply.github.com>
2023-03-05 23:51:08 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
-function saludo = prompt("Ingrese su nombre")
-alert ("Bienvenido a mi tarea 12 de js")
+let saludo = prompt("Ingrese su nombre");
+alert ("Bienvenido a mi tarea 12 de js");
+
+console.log(saludo);
 
